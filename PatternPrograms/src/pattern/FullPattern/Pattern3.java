6ade04b8c61aff132f3807d9fbe77b266ac1779e
VamshiReddy97 <bobalavamshidharreddy1996@gmail.com>
2022-04-22 12:51:43 +0530

    Pattern

---
+++ PatternPrograms/src/pattern/FullPattern/Pattern3.java
@@ -0,0 +1,16 @@
+package pattern.FullPattern;
+
+public class Pattern3 {
+	public static void main(String[] args)  
+    {   int i ,j;  
+        int n = 5;  
+            for(i = n; i>0 ; i-- )  
+            {  
+                 for(j = 1; j<=i ; j++)  
+                    {  
+                        System.out.print(j);  
+                    }  
+                         System.out.println("");  
+            }  
+    }  
+}
